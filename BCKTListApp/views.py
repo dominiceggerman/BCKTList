7e32408635358b5ba799e3884e31be83b52dcd6f
Dominic Eggerman <dominiceggerman@gmail.com>
2018-06-12 23:49:29 -0400

    Added DB query, context, and loop for bckt list items.

--- BCKTListApp/views.py
+++ BCKTListApp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Todo
 
 def index(request):
-    context = {}
+
+    # Get todo list from DB, assign to context
+    bckt_list = Todo.objects.order_by("id")
+    context = {"bckt_list" : bckt_list}
+    # Return
     return render(request, "index.html", context)
