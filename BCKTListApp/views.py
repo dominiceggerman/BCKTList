3d1b99795f36b22046e900083da955b6f8f41bba
Dominic Eggerman <dominiceggerman@gmail.com>
2018-06-16 13:26:35 -0400

    Added forms.

--- BCKTListApp/views.py
+++ BCKTListApp/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from .models import Todo
+from .forms import TodoForm
 
 def index(request):
 
-    # Get todo list from DB, assign to context
+    # Get todo from DB, make form, assign to context
     bckt_list = Todo.objects.order_by("id")
-    context = {"bckt_list" : bckt_list}
+    form = TodoForm()
+    context = {"bckt_list" : bckt_list, "form": form}
     # Return
     return render(request, "index.html", context)
